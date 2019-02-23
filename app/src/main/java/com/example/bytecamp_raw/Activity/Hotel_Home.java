63fd59c27c3a8db6cc4e32ad7b74e1f30f4437e2
KarthikRaj <naidukarthi2193@gmail.com>
2019-02-23 19:19:26 +0530

    "pushed"

--- app/src/main/java/com/example/bytecamp_raw/Activity/Hotel_Home.java
+++ app/src/main/java/com/example/bytecamp_raw/Activity/Hotel_Home.java
@@ -1,9 +1,12 @@
 package com.example.bytecamp_raw.Activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import com.example.bytecamp_raw.Adapter.hotel_home;
 import com.example.bytecamp_raw.Model.hotel_home_model;
@@ -16,6 +19,9 @@ public class Hotel_Home extends AppCompatActivity {
     private RecyclerView contributions;
     private RecyclerView.Adapter contriAdapter;
     private List<hotel_home_model> listItems;
+    private CardView card1;
+    private CardView card2;
+    private CardView card3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +39,21 @@ public class Hotel_Home extends AppCompatActivity {
         listItems.add(new hotel_home_model("16 Jan 2018","12:30 pm","COMPOST",3));
         contriAdapter = new hotel_home(this,listItems);
         contributions.setAdapter(contriAdapter);
+
+        card1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+////                Intent intent1 = new Intent(Hotel_Home.this,);
+//                intent1.putExtra("date","12 January 2018");
+//                intent1.putExtra("date","12:30 pm");
+//                intent1.putExtra("date",1);
+//                startActivity(intent1);
+            }
+        });
+
+
+
+
 
 
     }
