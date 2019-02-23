0c4afb8bf8d1d8c0bc0d4f13821b7f200512e7c7
KarthikRaj <naidukarthi2193@gmail.com>
2019-02-24 01:26:16 +0530

    login page

---
+++ app/src/main/java/com/example/bytecamp_raw/Activity/Login_Page.java
@@ -0,0 +1,15 @@
+package com.example.bytecamp_raw.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.bytecamp_raw.R;
+
+public class Login_Page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login__page);
+    }
+}
